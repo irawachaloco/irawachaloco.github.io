27bce77d4b09c4fa300b07501347c06d3b59b4b4
Oscar Ponce <wigahluk@gmail.com>
2015-05-30 14:42:09 -0700

    Adding directive for menu boxes

--- src/directives/index_menu_box.js
+++ src/directives/index_menu_box.js
@@ -2,23 +2,32 @@
     'use strict';
 
     angular.module('wPage').directive('indexMenuBox', [
-        function(){
+        '$location',
+        function($location){
             return {
                 restrict: 'E',
                 replace: 'true',
                 transclude: true,
                 templateUrl: 'src/views/directives/index_menu_box.html',
                 scope: {
-                    boxTitle: '@'
+                    boxTitle: '@',
+                    goTo: '@'
                 },
                 link: function (scope, element, attrs) {
                     scope.showContent = false;
                     var hasChildren = element.find('.box-content').children().length;
                     scope.toggleChildren = function () {
+                        navigate();
                         if (!hasChildren) return;
 
                         scope.showContent = !scope.showContent;
+                        scope.extraClass = scope.extraClass ? undefined : 'omar';
                     };
+                    function navigate () {
+                        if (scope.goTo) {
+                            $location.url(scope.goTo);
+                        }
+                    }
                 }
             };
         }]);
