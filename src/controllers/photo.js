6e155661a5527675209004f39b3ce5aa165c7575
“irawachaloco” <irawachaloco@gmail.com>
2015-06-14 18:22:33 -0500

    subí versión con carrusel

--- src/controllers/photo.js
+++ src/controllers/photo.js
@@ -4,7 +4,24 @@
     angular.module('wPage').controller('photo', [
         '$scope',
         function ($scope) {
-            $scope.name = 'My Name';
+            $scope.imageList = [
+                {
+                    title:'untitulo',
+                    url:'https://farm1.staticflickr.com/368/18287886786_5d05cc4c6d_b.jpg'
+                },
+                {
+                    title:'untitulo',
+                    url:'https://farm8.staticflickr.com/7735/18315501481_ffdbc6ffdc_b.jpg'
+                },
+                {
+                    title:'untitulo',
+                    url:'https://farm8.staticflickr.com/7758/17691287084_a8b326779c_b.jpg'
+                },
+                {
+                    title:'untitulo',
+                    url:'https://farm1.staticflickr.com/368/18287886786_5d05cc4c6d_b.jpg'
+                }
+            ];
         }
     ]);
 }(angular));
